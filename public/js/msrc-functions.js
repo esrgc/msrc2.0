213719f8d1c38b9020ea8d4dd08c0e250b11cdaf
cdflint <cflint420@comcast.net>
2017-02-06 08:02:04 -0500

    development push

--- public/js/msrc-functions.js
+++ public/js/msrc-functions.js
@@ -29,19 +29,35 @@ $(window).load(function() {
         return this;
     };
 
-    // init masonry js with imagesLoaded
-    var $container = $('#masonry-container').masonry({
-        columnWidth: '.grid-box',
+    // init masonry js with imagesLoaded for spotlight
+    var $spotlight = $('#masonry-container-spotlight').masonry({
+        columnWidth: '.grid-box-spotlight',
         itemSelector: '.gridItem',
         isFitWidth: true,
         percentPosition: true,
         isAnimated: true
     });
 
-    $container.masonryImagesReveal($('#images').find('.gridItem'));
+    $spotlight.masonryImagesReveal($('#images').find('.gridItem'));
 
-    var $gridItems = $('#masonry-container').masonry();
-    $gridItems.hide();
+    var $hideSpotlight = $('#masonry-container-spotlight').masonry();
+    $hideSpotlight.hide();
+    
+    // init masonry container for records
+    var $records = $('#masonry-container-records').masonry({
+        columnWidth: '.grid-box-records',
+        itemSelector: '.records-gridItem',
+        isFitWidth: true,
+        percentPosition: true,
+        isAnimated: true
+    });
+
+    $records.masonryImagesReveal($('#recordsImages').find('.records-gridItem'));
+	
+
+    
+    var $hideRecords = $('#masonry-container-records').masonry();
+    $hideRecords.hide();
 
 
 });
@@ -51,15 +67,26 @@ $(window).load(function() {
 $(window).resize(function() {
 
     // init masonry js with imagesLoaded
-    var $container = $('#masonry-container').masonry({
-        columnWidth: '.grid-box',
+    var $resizeSpotlight = $('#masonry-container-spotlight').masonry({
+        columnWidth: '.grid-box-spotlight',
         itemSelector: '.gridItem',
         isFitWidth: true,
         percentPosition: true,
         isAnimated: true
     });
 
-    $container.masonry();
+    $resizeSpotlight.masonry();
+
+    // init masonry js with imagesLoaded
+    var $resizeRecords = $('#masonry-container-records').masonry({
+        columnWidth: '.grid-box-records',
+        itemSelector: '.records-gridItem',
+        isFitWidth: true,
+        percentPosition: true,
+        isAnimated: true
+    });
+
+    $resizeRecords.masonry();
 
 });
 
@@ -121,16 +148,24 @@ $(document).ready(function() {
         //test to get jquery traverse tree
         */
         var attrID = $(this).attr('id');
-        var $gridItems = $('#masonry-container').masonry();
+        var $spotlight = $('#masonry-container-spotlight').masonry();
+        var $records = $('#masonry-container-records').masonry();
         //alert(attrID);
 
         if (attrID == 'btn1') {
-            $('#members-div > p').toggleClass('ac_collapse');
+            $(this).parent().siblings().find('#members2-div > figcaption').toggleClass('ac_collapse');
+            $(this).parent().find('#members-div > figcaption').toggleClass('ac_collapse');
+            
         } else if (attrID == 'btn2') {
-            $gridItems.slideToggle('slow');
-            $gridItems.masonry();
+        	$('.spotlight-wrapper').css({"z-index": "1000"});
+            $spotlight.slideToggle('slow');
+            $spotlight.masonry();
+        } else if (attrID == 'btn3') {
+        	$('.records-wrapper').css({"z-index": "1000"});
+            $records.slideToggle('slow');
+            $records.masonry();
         } else {
-            $('#records-div').toggleClass('ac_collapse');
+
         }
 
         /* the foo*/
@@ -153,17 +188,45 @@ $(document).ready(function() {
     });
 
 
-    var $container = $('#masonry-container').masonry();
+    var $clickSpotlight = $('#masonry-container-spotlight').masonry();
     // change size of item on click with toggle class
-    $container.on('click', '.gridItem', function(e) {
-    	e.preventDefault();
-    	$(this).toggleClass('grid-item--gigante');
-    	// trigger layout after item size changes
-    	$container.masonry();
+    $clickSpotlight.on('click', '.gridItem', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('grid-item--gigante');
+        $(this).children("#textBox").toggleClass("hidden");
+        // trigger layout after item size changes
+        $clickSpotlight.masonry();
+    });
+
+    var $clickRecords = $('#masonry-container-records').masonry();
+    // change size of item on click with toggle class
+    $clickRecords.on('click', '.records-gridItem', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('record-item--gigante');
+        $(this).children("#textBox").toggleClass("hidden");
+        // trigger layout after item size changes
+        $clickRecords.masonry();
     });
 
 
+    // setup expanding divs for voting members 
+    var $votingMem = $('#votingMem-button');
+    $votingMem.on('click', function(e) {
+    	e.preventDefault();
 
+    	//var attrID = $(this).attr('id');
+    	//alert(attrID);
+    	$("#votingMem").toggleClass("hidden");
+    });
+
+    // setup expanding divs for non-voting members 
+    var $nonVoting = $('#nonVoting-button');
+    $nonVoting.on('click', function(e) {
+    	e.preventDefault();
+
+    	//var attrID = $(this).attr('id');
+    	//alert(attrID);
+    	$("#nonVoting").toggleClass("hidden");
+    });
     //.end doc ready function
 });
-
