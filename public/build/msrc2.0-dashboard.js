cb6f0e28f33162f39dee4275fccb34f024be59ec
cdflint <cflint420@comcast.net>
2017-02-27 15:54:12 -0500

    development push

--- public/build/msrc2.0-dashboard.js
+++ public/build/msrc2.0-dashboard.js
@@ -631,13 +631,11 @@ $(document).ready(function() {
     if (navigator.userAgent.match(/Firefox\/51.0/i)) {
         //$("body").css( "font-size", "18px" );
         $("#logo_lrg").css({
-            "font-size": '1.625em',
-            "-moz-transform": 'rotate(0.01)'
-    });
+            "font-size": '1.625em'
+        });
         $("#logo_med").css({
-            "font-size": '1.375em',
-            "-moz-transform": 'rotate(0.01)'
-    });
+            "font-size": '1.375em'    
+        });
         $("body").flowtype({
             minFont: 6,
             maxFont: 18,
