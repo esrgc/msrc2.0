1244429ff2de11760b14332eb58febec5934e586
cdflint <cflint420@comcast.net>
2017-02-27 08:27:24 -0500

    development push

--- public/build/msrc2.0-dashboard.js
+++ public/build/msrc2.0-dashboard.js
@@ -421,33 +421,36 @@ $(document).ready(function() {
 //     document.getElementById(location.hash).click();
 //   }
 // }  
-
 // window.onhashchange = locationHashChanged;
 
-// $(window).on('hashchange', function(e) {
-//     e.preventDefault();
-//     //e.stopPropagation();
+////////////////////////////////////////////////////////////////////////////////////////////
 
-//     if (location.hash == "#comCEDS" || location.hash == "#comEM" || location.hash == "#comGIS" || location.hash == "#comIT" || location.hash == "#comMUST") {
-//         $('#advisGroup').trigger('click');
-//         $(location.hash).trigger('click');
+// functional hash location change function
 
-//     } else if (location.hash == "#members" || location.hash == "#spotlight" || location.hash == "#records") {
-//         /// cross browser functionality issues with firefox and msie
-//         // relative position elements become thrown to the left when open and closed
-//         //var x = location.hash;
-//         $(location.hash).trigger('click');
-//     } else {
-//         $(location.hash).trigger('click');
-//     }
+$(window).on('hashchange', function(e) {
+    e.preventDefault();
+    //e.stopPropagation();
 
-// });
+    if (location.hash == "#comCEDS" || location.hash == "#comEM" || location.hash == "#comGIS" || location.hash == "#comIT" || location.hash == "#comMUST") {
+        $('#advisGroup').trigger('click');
+        $(location.hash).trigger('click');
 
-// $(window).load(function() {
-//     if (window.location.hash) {
-//         $(window).trigger('hashchange');
-//     }
-// });
+    } else if (location.hash == "#members" || location.hash == "#spotlight" || location.hash == "#records") {
+        /// cross browser functionality issues with firefox and msie
+        // relative position elements become thrown to the left when open and closed
+        //var x = location.hash;
+        $(location.hash).trigger('click');
+    } else {
+        $(location.hash).trigger('click');
+    }
+
+});
+
+$(window).load(function() {
+    if (window.location.hash) {
+        $(window).trigger('hashchange');
+    }
+});
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
