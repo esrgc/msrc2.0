55600fadc45891df046b01f308cc3868e7d25a33
cdflint <cflint420@comcast.net>
2017-02-27 14:38:15 -0500

    development push

--- public/build/msrc2.0-dashboard.js
+++ public/build/msrc2.0-dashboard.js
@@ -626,3 +626,34 @@ $(document).ready(function() {
 
 });
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// browser compatibility solution for flowType.js
+// mozilla has HUGE!!!! H1-H6 with flow type 
+// This is to tone it down a thousand notches
+
+
+$(document).ready(function() {
+    if (navigator.userAgent.match(/Firefox\/51.0/i)) {
+        $("body").css({ "font-size": "18px" });
+        $("body").flowtype({
+            minFont: 6,
+            maxFont: 18,
+            fontRatio: 65
+        });
+        $("#logo_lrg").css("font-size", '1.6em');
+
+        $("#logo_med").css("font-size", '1.3em');
+
+        //console.log('this is firefox!');
+    } else {
+        $("body").flowtype({
+            minFont: 6,
+            maxFont: 18,
+            fontRatio: 65
+        });
+    }
+});
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
