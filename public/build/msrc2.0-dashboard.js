3d8194ec98b36b517592ac78e658b4b16f176868
cdflint <cflint420@comcast.net>
2017-02-27 14:42:32 -0500

    development push

--- public/build/msrc2.0-dashboard.js
+++ public/build/msrc2.0-dashboard.js
@@ -636,14 +636,15 @@ $(document).ready(function() {
 $(document).ready(function() {
     if (navigator.userAgent.match(/Firefox\/51.0/i)) {
         $("body").css({ "font-size": "18px" });
+        $("#logo_lrg").css("font-size", '1.65em');
+        $("#logo_med").css("font-size", '1.32em');
+
         $("body").flowtype({
             minFont: 6,
             maxFont: 18,
             fontRatio: 65
         });
-        $("#logo_lrg").css("font-size", '1.6em');
 
-        $("#logo_med").css("font-size", '1.3em');
 
         //console.log('this is firefox!');
     } else {
