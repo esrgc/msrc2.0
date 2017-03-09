b1d38d6d1ee262f0628ec9232e7d16f182cb5c7e
cdflint <cflint420@comcast.net>
2017-03-09 12:05:28 -0500

    :japanese_goblin: fix typo

--- public/build/msrc2.0-dashboard.js
+++ public/build/msrc2.0-dashboard.js
@@ -2,6 +2,7 @@
 $(window).load(function() {
     // for my first trick now you see them...no you dont
     // hide alot of things that can open later
+    
     $(".footer").hide();
     $('.comMenu').hide();
     $("#execDiv").hide();
--- routes/index.js
+++ routes/index.js
@@ -2,47 +2,48 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.use(function(req, res, next){
-	res.locals.path = req.path.replace('/', '');
-	console.log('Path: ....');
-	console.log(res.locals.path);
-	next();
+router.use(function(req, res, next) {
+    res.locals.path = req.path.replace('/', '');
+    console.log('Path: ....');
+    console.log(res.locals.path);
+    next();
 });
 
 /* GET home page. */
-router.get('/', function(req, res){
-	res.redirect('index');
+router.get('/', function(req, res) {
+    res.redirect('index');
 });
 
 //home page
-router.get('/index', function(req, res){
-	res.render('index', { title: 'Home'});	
+router.get('/index', function(req, res) {
+    res.render('index', { title: 'Home' });
 });
 
 //About
-router.get('/about', function(req, res){
-	res.render('about', { title: 'About'});	
+router.get('/about', function(req, res) {
+    res.render('about', { title: 'About' });
 });
 
 //committees
-router.get('/committees', function(req, res){
-	res.render('committees', { title: 'Committees'});	
+router.get('/committees', function(req, res) {
+    res.render('committees', { title: 'Committees' });
 });
 
 //partners
-router.get('/partners', function(req, res){
-	res.render('partners', { title: 'Partners'});	
+router.get('/partners', function(req, res) {
+    res.render('partners', { title: 'Partners' });
 });
 
 /// remove from routes when complete
-router.get('/testing', function(req, res){
-	res.render('testing', { title: 'testing'});	
+router.get('/testing', function(req, res) {
+    res.render('testing', { title: 'testing' });
 });
 
 /// resources page
-router.get('/resources', function(req, res){
-	res.render('resources', { title: 'Resources'});	
+router.get('/resources', function(req, res) {
+    res.render('resources', { title: 'Resources' });
 });
+
 
 
 
