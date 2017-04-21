16176c5931b1f391252126ffbc496d6638d41cf4
cdflint <cxflint@gmail.com>
2017-04-21 08:11:22 -0400

    :squirrel: module organization

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+// bringing on the scripts for webpack
+import './scritps/onLoad_Hide';
+import './scritps/mobileNav';
+import './scritps/DocReady';
+import './scritps/footer-hash_Router';
+//test to have webpack compile my less
+import '../css/style.less';
