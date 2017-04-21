a04d32bef0e6c82bb4f155d3304532298b79e014
cdflint <cxflint@gmail.com>
2017-04-21 09:05:01 -0400

    :squirrel: new dev branch for improvements

--- src/js/about/scripts/members.js
+++ src/js/about/scripts/members.js
@@ -1,47 +1,48 @@
 /* setup expanding divs for voting members*/
 var $votingMem = $('#votingMem-button');
 $votingMem.on('click', function(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    /*var attrID = $(this).attr('id');*/
-    /*alert(attrID);*/
-    if ($("#nonVoting").is(":visible")) {
-        $("#nonVoting, .membersDiv2_bg").toggle("slide");
-    }
+  /*var attrID = $(this).attr('id');*/
+  /*alert(attrID);*/
+  if ($("#nonVoting").is(":visible")) {
+    $("#nonVoting, .membersDiv2_bg").toggle("slide");
+  }
 
-    if ($("#votingMem").is(":hidden")) {
+  if ($("#votingMem").is(":hidden")) {
 
-        $("#membersDiv2").animate({"z-index": "1000", "opacity": "1"});
+    $("#membersDiv2").animate({"z-index": "1000", "opacity": "1"});
 
-        $("#membersDiv2, #votingMem, .membersDiv2_bg").fadeIn(875);
+    $("#membersDiv2, #votingMem, .membersDiv2_bg").fadeIn(875);
 
-    } else {
+  } else {
 
-        $("#membersDiv2, #votingMem, .membersDiv2_bg").fadeOut(850);
-        $("#membersDiv2").animate({"z-index": "0", "opacity": "0"});
+    $("#membersDiv2, #votingMem, .membersDiv2_bg").fadeOut(850);
+    
+    $("#membersDiv2").animate({"z-index": "0", "opacity": "0"});
 
-    }
+  }
 });
 
 /* setup expanding divs for non-voting members*/
 var $nonVoting = $('#nonVoting-button');
 $nonVoting.on('click', function(e) {
-    e.preventDefault();
-    /*var attrID = $(this).attr('id');*/
-    /*alert(attrID);*/
-    if ($("#votingMem").is(":visible")) {
-        $("#votingMem, .membersDiv2_bg").toggle("slide");
-    }
+  e.preventDefault();
+  /*var attrID = $(this).attr('id');*/
+  /*alert(attrID);*/
+  if ($("#votingMem").is(":visible")) {
+    $("#votingMem, .membersDiv2_bg").toggle("slide");
+  }
 
-    if ($("#nonVoting").is(":hidden")) {
+  if ($("#nonVoting").is(":hidden")) {
 
-        $("#membersDiv2").css({"z-index": "1000", "opacity": "1"});
+    $("#membersDiv2").css({"z-index": "1000", "opacity": "1"});
 
-        $("#membersDiv2, #nonVoting, .membersDiv2_bg").fadeIn(875);
-    } else {
-        $("#membersDiv2, #nonVoting, .membersDiv2_bg").fadeOut(850);
+    $("#membersDiv2, #nonVoting, .membersDiv2_bg").fadeIn(875);
+  } else {
+    $("#membersDiv2, #nonVoting, .membersDiv2_bg").fadeOut(850);
 
-        $("#membersDiv2").css({"z-index": "0", "opacity": "0"});
-    }
+    $("#membersDiv2").css({"z-index": "0", "opacity": "0"});
+  }
 
 });
