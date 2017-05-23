d0d9d5f183174434c44ef7129aaaa6c094538e2f
Tu Hoang <dhoang519@gmail.com>
2017-05-23 17:07:34 -0400

    changes to the members

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var hbs = require('express-hbs');
 var routes = require('./routes/index');
-var config = require('./bin/config');
+// var config = require('./bin/config');
 
 var app = express();
 
