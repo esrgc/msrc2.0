005c7c9909ad1e909aa6017f579956a02321fdfb
Tu Hoang <dhoang519@gmail.com>
2017-07-10 16:01:37 -0400

    added FY18 work plan doc

--- public/build/home-bundle.js
+++ public/build/home-bundle.js
@@ -1,1 +1,168 @@
-webpackJsonp([3],{135:function(t,o,e){"use strict";(function(t){t(".carousel").carousel({interval:5e3})}).call(o,e(1))},136:function(t,o,e){"use strict";(function(t){t(window).scroll(function(){t(this).scrollTop()>250?t("#to-top").stop(!0,!0).fadeIn():t("#to-top").stop(!0,!0).fadeOut()})}).call(o,e(1))},141:function(t,o,e){"use strict";(function(t){e(23),e(22);var o=e(24);e(136),e(135),e(25),t("i#mobileNav").on("click",function(){(0,o.mobileNav)()})}).call(o,e(1))},22:function(t,o,e){"use strict";(function(t){t(".footer").addClass("hidden"),t(".comMenu").addClass("hidden"),t("#footer_button").on("click",function(o){o.preventDefault(),t(".footer").toggleClass("hidden")}),t("#dropdownMenuButton").click(function(o){return o.preventDefault(),t(this).find(".toggle-close").toggleClass("toggle-open"),t(".comMenu").toggleClass("hidden"),!!t(this).hasClass("toggle-open")&&void t(this).removeClass("toggle-open").slideUp(500)})}).call(o,e(1))},23:function(t,o,e){"use strict";(function(t){navigator.userAgent.match(/Firefox/i)?(t("#logoBrand").css({"padding-top":"10px"}),t("#imgcontainer, .textBox-body, .minWrapper").css({"padding-bottom":"140px"}),t(".textBox").css({"padding-bottom":"30px"}),t("#logo_lrg").css({"line-height":"35px","font-size-adjust":"0.275"}),t("#logo_sm").css({"line-height":"20px","font-size-adjust":"0.375"})):navigator.userAgent.match(/Trident/i)&&(e(57),t("#imgcontainer, .textBox-body, .minWrapper").css({"padding-bottom":"130px"}),t(".textBox").css({"padding-bottom":"30px"}))}).call(o,e(1))},24:function(t,o,e){"use strict";function mobileNav(){var t=document.getElementById("myTopnav");"nav navbar-nav navbar-right text-vertical-center"===t.className?t.className+=" responsive":t.className="nav navbar-nav navbar-right text-vertical-center"}Object.defineProperty(o,"__esModule",{value:!0}),o.mobileNav=mobileNav},25:function(t,o){}},[141]);
+webpackJsonp([3],{
+
+/***/ 135:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function($) {
+
+/*set home page carousel rotation speed*/
+$('.carousel').carousel({
+    interval: 5000 //set speed in ms
+});
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
+
+/***/ }),
+
+/***/ 136:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function($) {
+
+/*to top button appeasrs after scroll down*/
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 250) {
+    $('#to-top').stop(true, true).fadeIn();
+  } else {
+    $('#to-top').stop(true, true).fadeOut();
+  }
+});
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
+
+/***/ }),
+
+/***/ 141:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function($) {
+
+__webpack_require__(23);
+
+__webpack_require__(22);
+
+var _mobileNav = __webpack_require__(24);
+
+__webpack_require__(136);
+
+__webpack_require__(135);
+
+__webpack_require__(25);
+
+// for small screens
+$('i#mobileNav').on('click', function () {
+  (0, _mobileNav.mobileNav)();
+});
+
+/* home page specific functions */
+/*import global js functions*/
+
+
+/*import css*/
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
+
+/***/ }),
+
+/***/ 22:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function($) {
+
+//
+// Author: Carl Flint
+// ESRGC
+//
+/* hide the nav footer on page load */
+$(".footer").addClass("hidden");
+/* hide the drop down advisory groups on page load */
+$(".comMenu").addClass("hidden");
+/* footer slide toggle nav pane */
+$("#footer_button").on('click', function (e) {
+    e.preventDefault();
+    $('.footer').toggleClass("hidden");
+    return;
+});
+
+/* footer dropdown menu for advisory committees*/
+$("#dropdownMenuButton").click(function (e) {
+    e.preventDefault();
+
+    $(this).find('.toggle-close').toggleClass('toggle-open');
+    $(".comMenu").toggleClass('hidden');
+
+    if ($(this).hasClass("toggle-open")) {
+        $(this).removeClass("toggle-open").slideUp(500);
+        return;
+    }
+    return false;
+});
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
+
+/***/ }),
+
+/***/ 23:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function($) {
+
+/*browser compatibility solution for flowType.js and fixed position elements not rendering the same*/
+/*mozilla renders the google font HUGE!!!!*/
+/*This is to tone it down a thousand notches*/
+if (navigator.userAgent.match(/Firefox/i)) {
+    $("#logoBrand").css({ "padding-top": "10px" });
+    $("#imgcontainer, .textBox-body, .minWrapper").css({ "padding-bottom": "140px" });
+    $(".textBox").css({ "padding-bottom": "30px" });
+
+    $("#logo_lrg").css({ "line-height": '35px', "font-size-adjust": "0.275" });
+    $("#logo_sm").css({ "line-height": '20px', "font-size-adjust": "0.375" });
+
+    // console.log('this is firefox!');
+} else if (navigator.userAgent.match(/Trident/i)) {
+    __webpack_require__(57);
+    $("#imgcontainer, .textBox-body, .minWrapper").css({ "padding-bottom": "130px" });
+    $(".textBox").css({ "padding-bottom": "30px" });
+
+    // console.log('this is internetExplorer');
+} else {}
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
+
+/***/ }),
+
+/***/ 24:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.mobileNav = mobileNav;
+//
+// Author: Carl Flint, ESRGC
+//
+
+/* responsive nav function */
+function mobileNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "nav navbar-nav navbar-right text-vertical-center") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav navbar-nav navbar-right text-vertical-center";
+    }
+}
+
+/***/ }),
+
+/***/ 25:
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+
+},[141]);
+//# sourceMappingURL=home-bundle.js.map
--- public/build/partners-bundle.js
+++ public/build/partners-bundle.js
@@ -1,1 +1,240 @@
-webpackJsonp([2],{137:function(n,o,t){"use strict";(function(n){var o=t(46);n("figure#DCED").on("click",function(){(0,o.downloadLink)("http://choosedorchester.org/")}),n("figure#CCED").on("click",function(){(0,o.downloadLink)("https://www.carolinebusiness.com")}),n("figure#TCED").on("click",function(){(0,o.downloadLink)("http://www.talbotcountymd.gov/index.php?page=Economic_Development")}),n("figure#USEDA").on("click",function(){(0,o.downloadLink)("http://www.eda.gov/")}),n("figure#MDBED").on("click",function(){(0,o.downloadLink)("http://www.choosemaryland.org/")}),n("figure#MUSTBUS").on("click",function(){(0,o.downloadLink)("http://www.mustbus.org/")}),n("figure#ESEC").on("click",function(){(0,o.downloadLink)("http://www.ventureahead.org/")}),n("figure#MDBC").on("click",function(){(0,o.downloadLink)("http://www.mdbc.us/")}),n("figure#ESRGC").on("click",function(){(0,o.downloadLink)("http://www.esrgc.org/")}),n("figure#MDFARM").on("click",function(){(0,o.downloadLink)("http://www.marylandfarming.com/")}),n("figure#BEACON").on("click",function(){(0,o.downloadLink)("http://beacon.salisbury.edu/")}),n("figure#MDDLLR").on("click",function(){(0,o.downloadLink)("http://www.dllr.state.md.us/")})}).call(o,t(1))},142:function(n,o,t){"use strict";(function(n){t(23),t(22);var o=t(24);t(137),t(25),n("i#mobileNav").on("click",function(){(0,o.mobileNav)()})}).call(o,t(1))},22:function(n,o,t){"use strict";(function(n){n(".footer").addClass("hidden"),n(".comMenu").addClass("hidden"),n("#footer_button").on("click",function(o){o.preventDefault(),n(".footer").toggleClass("hidden")}),n("#dropdownMenuButton").click(function(o){return o.preventDefault(),n(this).find(".toggle-close").toggleClass("toggle-open"),n(".comMenu").toggleClass("hidden"),!!n(this).hasClass("toggle-open")&&void n(this).removeClass("toggle-open").slideUp(500)})}).call(o,t(1))},23:function(n,o,t){"use strict";(function(n){navigator.userAgent.match(/Firefox/i)?(n("#logoBrand").css({"padding-top":"10px"}),n("#imgcontainer, .textBox-body, .minWrapper").css({"padding-bottom":"140px"}),n(".textBox").css({"padding-bottom":"30px"}),n("#logo_lrg").css({"line-height":"35px","font-size-adjust":"0.275"}),n("#logo_sm").css({"line-height":"20px","font-size-adjust":"0.375"})):navigator.userAgent.match(/Trident/i)&&(t(57),n("#imgcontainer, .textBox-body, .minWrapper").css({"padding-bottom":"130px"}),n(".textBox").css({"padding-bottom":"30px"}))}).call(o,t(1))},24:function(n,o,t){"use strict";function mobileNav(){var n=document.getElementById("myTopnav");"nav navbar-nav navbar-right text-vertical-center"===n.className?n.className+=" responsive":n.className="nav navbar-nav navbar-right text-vertical-center"}Object.defineProperty(o,"__esModule",{value:!0}),o.mobileNav=mobileNav},25:function(n,o){},46:function(n,o,t){"use strict";(function(n){function downloadLink(n){window.open(n,"_blank")}Object.defineProperty(o,"__esModule",{value:!0}),o.downloadLink=downloadLink,n("a#workPlanLink").on("click",function(){downloadLink("./documents/msrc/MSRC_FY17_Work_Plan.pdf")}),n("a#byLawsLink").on("click",function(){downloadLink("./documents/msrc/MSRC_Bylaws.pdf")}),n("a#CEDSLink").on("click",function(){downloadLink("./documents/reports/CEDS_Full_Document_revised_3-9-17.pdf")}),n("a#broadbandLink").on("click",function(){downloadLink("./documents/reports/Upper_Eastern_Shore_Region_2012.pdf")}),n("a#MUSTBUS").on("click",function(){downloadLink("http://www.mustbus.org/")}),n("a#ESRGC").on("click",function(){downloadLink("http://www.esrgc.org/")})}).call(o,t(1))}},[142]);
+webpackJsonp([2],{
+
+/***/ 137:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function($) {
+
+var _downloadLink = __webpack_require__(46);
+
+$('figure#DCED').on('click', function () {
+  (0, _downloadLink.downloadLink)('http://choosedorchester.org/');
+});
+
+$('figure#CCED').on('click', function () {
+  (0, _downloadLink.downloadLink)('https://www.carolinebusiness.com');
+});
+
+$('figure#TCED').on('click', function () {
+  (0, _downloadLink.downloadLink)('http://www.talbotcountymd.gov/index.php?page=Economic_Development');
+});
+
+$('figure#USEDA').on('click', function () {
+  (0, _downloadLink.downloadLink)('http://www.eda.gov/');
+});
+
+$('figure#MDBED').on('click', function () {
+  (0, _downloadLink.downloadLink)('http://www.choosemaryland.org/');
+});
+
+$('figure#MUSTBUS').on('click', function () {
+  (0, _downloadLink.downloadLink)('http://www.mustbus.org/');
+});
+
+$('figure#ESEC').on('click', function () {
+  (0, _downloadLink.downloadLink)('http://www.ventureahead.org/');
+});
+
+$('figure#MDBC').on('click', function () {
+  (0, _downloadLink.downloadLink)('http://www.mdbc.us/');
+});
+
+$('figure#ESRGC').on('click', function () {
+  (0, _downloadLink.downloadLink)('http://www.esrgc.org/');
+});
+
+$('figure#MDFARM').on('click', function () {
+  (0, _downloadLink.downloadLink)('http://www.marylandfarming.com/');
+});
+
+$('figure#BEACON').on('click', function () {
+  (0, _downloadLink.downloadLink)('http://beacon.salisbury.edu/');
+});
+
+$('figure#MDDLLR').on('click', function () {
+  (0, _downloadLink.downloadLink)('http://www.dllr.state.md.us/');
+});
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
+
+/***/ }),
+
+/***/ 142:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function($) {
+
+__webpack_require__(23);
+
+__webpack_require__(22);
+
+var _mobileNav = __webpack_require__(24);
+
+__webpack_require__(137);
+
+__webpack_require__(25);
+
+// for small screens
+$('i#mobileNav').on('click', function () {
+  (0, _mobileNav.mobileNav)();
+});
+
+/* import logo router */
+/* import global js functions*/
+
+
+/* import css*/
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
+
+/***/ }),
+
+/***/ 22:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function($) {
+
+//
+// Author: Carl Flint
+// ESRGC
+//
+/* hide the nav footer on page load */
+$(".footer").addClass("hidden");
+/* hide the drop down advisory groups on page load */
+$(".comMenu").addClass("hidden");
+/* footer slide toggle nav pane */
+$("#footer_button").on('click', function (e) {
+    e.preventDefault();
+    $('.footer').toggleClass("hidden");
+    return;
+});
+
+/* footer dropdown menu for advisory committees*/
+$("#dropdownMenuButton").click(function (e) {
+    e.preventDefault();
+
+    $(this).find('.toggle-close').toggleClass('toggle-open');
+    $(".comMenu").toggleClass('hidden');
+
+    if ($(this).hasClass("toggle-open")) {
+        $(this).removeClass("toggle-open").slideUp(500);
+        return;
+    }
+    return false;
+});
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
+
+/***/ }),
+
+/***/ 23:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function($) {
+
+/*browser compatibility solution for flowType.js and fixed position elements not rendering the same*/
+/*mozilla renders the google font HUGE!!!!*/
+/*This is to tone it down a thousand notches*/
+if (navigator.userAgent.match(/Firefox/i)) {
+    $("#logoBrand").css({ "padding-top": "10px" });
+    $("#imgcontainer, .textBox-body, .minWrapper").css({ "padding-bottom": "140px" });
+    $(".textBox").css({ "padding-bottom": "30px" });
+
+    $("#logo_lrg").css({ "line-height": '35px', "font-size-adjust": "0.275" });
+    $("#logo_sm").css({ "line-height": '20px', "font-size-adjust": "0.375" });
+
+    // console.log('this is firefox!');
+} else if (navigator.userAgent.match(/Trident/i)) {
+    __webpack_require__(57);
+    $("#imgcontainer, .textBox-body, .minWrapper").css({ "padding-bottom": "130px" });
+    $(".textBox").css({ "padding-bottom": "30px" });
+
+    // console.log('this is internetExplorer');
+} else {}
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
+
+/***/ }),
+
+/***/ 24:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.mobileNav = mobileNav;
+//
+// Author: Carl Flint, ESRGC
+//
+
+/* responsive nav function */
+function mobileNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "nav navbar-nav navbar-right text-vertical-center") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav navbar-nav navbar-right text-vertical-center";
+    }
+}
+
+/***/ }),
+
+/***/ 25:
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 46:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function($) {
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.downloadLink = downloadLink;
+/*download document function based on jquery selector*/
+/*downloadable pdf will open in new windows*/
+/*word.doc and .docx will download without window change*/
+function downloadLink(url) {
+  window.open(url, '_blank');
+}
+
+/* setup downloadable links for pdf documents for use on multiple pages */
+// link downloadable document to anchor with id workPlanLink
+$('a#workPlanLink').on('click', function () {
+  downloadLink('./documents/msrc/MSRC_FY17_Work_Plan.pdf');
+});
+// link downloadable document to anchor with id byLawsLink
+$('a#byLawsLink').on('click', function () {
+  downloadLink('./documents/msrc/MSRC_Bylaws.pdf');
+});
+// link downloadable document to anchor with id CEDSLink
+$('a#CEDSLink').on('click', function () {
+  downloadLink('./documents/reports/CEDS_Full_Document_revised_3-9-17.pdf');
+});
+// link downloadable document to anchor with id broadbandLink
+$('a#broadbandLink').on('click', function () {
+  downloadLink('./documents/reports/Upper_Eastern_Shore_Region_2012.pdf');
+});
+//
+//
+/* setup external links for advisory committee page*/
+$('a#MUSTBUS').on('click', function () {
+  downloadLink('http://www.mustbus.org/');
+});
+$('a#ESRGC').on('click', function () {
+  downloadLink('http://www.esrgc.org/');
+});
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
+
+/***/ })
+
+},[142]);
+//# sourceMappingURL=partners-bundle.js.map
